f9d9cf58ecd19649276fa74f78405894b0326926
24f2002843 <24f2002843@ds.study.iitm.ac.in>
2025-10-15 13:58:30 +0530

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// Placeholder for Service Worker code if needed in future rounds
+self.addEventListener('install', (event) => {
+    console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker: Activated');
+});
